731e85fe26520b07a2382e65a489120716b556c8
stefschr <stefschr@iu.edu>
2024-04-03 11:58:49 -0400

    .

---
+++ src/main/java/com/fishinggapp/fishingappbackend/FishingAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fishinggapp.fishingappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishingAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fishinggapp/fishingappbackend/FishingAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fishinggapp.fishingappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishingAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
